31bd03d9b6c8c307257cb438bf094cbf307823fb
NinAlien <ninastephanie.som@outlook.fr>
2018-03-26 15:26:15 +0200

    Version_1:

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function loadXMLDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+            this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://api.deezer.com/version/service/id/method/?parameters", true);
+    xhttp.send();
+}
+DZ.init({
+    appId  : 275242,
+    channelUrl : 'http://127.0.0.1:8080/'
+});
+DZ.login(function(response) {
+    if (response.authResponse) {
+        console.log('Welcome! Fetching your information.... ');
+        DZ.api('/user/me', function(response) {
+            console.log('Good to see you, ' + response.name + '.');
+        });
+    } else {
+        console.log('User cancelled login or did not fully authorize.');
+    }
+}, {perms: 'basic_access,email'});
