76b26b36aa897652bac24d07e1b7ffe069f05548
NinAlien <ninastephanie.som@outlook.fr>
2018-03-30 16:42:10 +0200

    30.03.18

---
+++ js/deezweb.js
@@ -0,0 +1,93 @@
+Vue.component('recherche', {
+    data : function() {
+        return {
+            urlSrc:'https://api.deezer.com/search?q=',
+            getInfo:'',
+            index: 0,
+            artistInfo:[],
+            orderOption:[],
+            favourite : []
+        }
+    },
+    methods : {
+        display: function(){
+            $.ajax({
+                url:'https://api.deezer.com/search?q='+this.getInfo,
+                dataType : 'jsonp', // <-- Informe jQuery qu'il recevra du JSONP de la part de Deezer
+                data : {
+                    output : 'jsonp', // <-- Indique à Deezer que le format de retour doit être du JSONP
+                }
+            }).then(response=>{
+                this.artistInfo = response.data;
+                console.log(response.data);
+                return this.artistInfo;
+            }, function() {
+                console.log("nope");
+            });
+        }
+    },
+    template : 
+        `<div>
+            <div class="collection with-header">
+                <h1 class="collection-header">Page de recherche</h1>
+                <div class="collection-item">
+                    <form class="row">
+                        <div class="input-field">
+                            <label for="deezSearch">Rechercher un artiste ...</label>
+                            <input v-model="getInfo" type="text" placeholder="">
+                        </div>
+                        <button @click.prevent="display()" class="btn waves-effect">Rechercher</button>
+                    </form>
+                </div>
+            </div>
+            <div v-for="artist in artistInfo">
+                <div class="row">
+                    <div v-for="(artist, index) in artistInfo" class="col s3 card hoverable">
+                        <div class="card-image img-container">
+                            <img class="activator" :src="artist.album.cover_big" alt="Couverture de l'album">
+                        </div>
+                        <div class="card-content">
+                            <h2 class="truncate card-title activator">{{artist.title}}</h2>
+                            <blockquote class="truncate grey-text text-darken-4">{{artist.artist.name}}<br>{{artist.album.title}}</blockquote>
+                            <audio controls controlsList="nodownload">
+                                <source :src="artist.preview" type="audio/mpeg">
+                            </audio>
+                            <i class="small material-icons favorite_border right-align">favorite_border</i>
+                        </div>
+                        <div class="card-action">
+                            <a @click="trackpage" class="waves-effect waves-light green accent-3 btn">Voir les détails du titre</a>
+                            <a @click="albumpage" class="waves-effect waves-light green accent-3 btn">Consulter l’album</a>
+                            <a @click="artistpage" class="waves-effect waves-light green accent-3 btn">Voir la fiche de l’artiste</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+});
+
+Vue.component('trackpage', {
+    template :
+        `<div class="collection with-header">
+            <div v-for="(artist, index) in artistInfo" class="col s3 card hoverable">
+            <div class="card-image img-container">
+                <img class="activator" :src="artist.album.cover_big" alt="Couverture de l'album">
+            </div>
+            <div class="card-content">
+                <h2 class="truncate card-title activator">{{artist.title}}</h2>
+                <blockquote class="truncate grey-text text-darken-4">{{artist.artist.name}}<br>{{artist.album.title}}</blockquote>
+                <audio controls controlsList="nodownload">
+                    <source :src="artist.preview" type="audio/mpeg">
+                </audio>
+                <i class="small material-icons favorite_border right-align">favorite_border</i>
+            </div>
+            <div class="card-action">
+                <a @click="trackpage" class="waves-effect waves-light green accent-3 btn">Voir les détails du titre</a>
+                <a @click="albumpage" class="waves-effect waves-light green accent-3 btn">Consulter l’album</a>
+                <a @click="artistpage" class="waves-effect waves-light green accent-3 btn">Voir la fiche de l’artiste</a>
+            </div>
+        </div>`
+});
+
+var homePage = new Vue({
+    el:'#deezer-app',
+});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-function loadXMLDoc() {
+/* function jsonp(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    var script = document.createElement('script');
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
+
+jsonp('https://api.deezer.com/version/service/id/method/?parameters', function(data) {
+    alert(data);
+}); */
+
+
+
+/* function loadXMLDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -8,7 +27,9 @@ function loadXMLDoc() {
     };
     xhttp.open("GET", "https://cors-anywhere.herokuapp.com/https://api.deezer.com/version/service/id/method/?parameters", true);
     xhttp.send();
-}
+} 
+
+
 DZ.init({
     appId  : 275242,
     channelUrl : 'http://127.0.0.1:8080/'
@@ -22,4 +43,4 @@ DZ.login(function(response) {
     } else {
         console.log('User cancelled login or did not fully authorize.');
     }
-}, {perms: 'basic_access,email'});
+}, {perms: 'basic_access,email'});*/
